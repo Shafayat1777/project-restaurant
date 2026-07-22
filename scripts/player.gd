1b1bb8ec589621e7cb1e9f689bfa0eca0d3d5a76
shafayat <shafayat1777@gmail.com>
2026-07-23 02:43:15 +0600

    added fixed animations for movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+var last_direction: Vector2 = Vector2.RIGHT
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 
 	process_movement()
 	move_and_slide()
@@ -15,13 +16,19 @@ func process_movement() -> void:
 
 	var direction := Input.get_vector("left", "right", "up", "down")
 
-	velocity = direction * SPEED
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
 	
-	play_animation(direction)
+	process_animatin(last_direction)
 
-func process_animatin() -> void:
+func process_animatin(dir: Vector2) -> void:
 	if velocity != Vector2.ZERO:
-		play_animation()
+		play_animation("walk", dir)
+	else:
+		play_animation("idle", dir)
 	
 
 func play_animation(prefix: String, dir: Vector2) -> void:
@@ -30,6 +37,6 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 	elif dir.x < 0:
 		animated_sprite_2d.play(prefix + "_left")
 	elif dir.y > 0:
-		animated_sprite_2d.play(prefix + "_stand")
+		animated_sprite_2d.play(prefix + "_down")
 	elif dir.y < 0:
 		animated_sprite_2d.play(prefix + "_up")
