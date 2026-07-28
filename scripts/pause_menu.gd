a578333072408f8821c61321eb8c0090044c5a54
Shafqat <shafkatananta122@gmail.com>
2026-07-29 04:06:50 +0600

    Added Pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
+		resume()
+		
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_options_pressed() -> void:
+	print("Options")
+
+
+func _on_exit_to_main_menu_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _process(delta):
+	testEsc()
