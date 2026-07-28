b986102a3e0958d8c1da185f33aecbb311da2e55
shafayat <shafayat1777@gmail.com>
2026-07-29 00:43:29 +0600

    added new outline fix

--- interaction_components/interactable.gd
+++ interaction_components/interactable.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 @export var interact_name: String = ""
 @export var is_intractable: bool = false
+@export var sprite: AnimatedSprite2D
 
 var interact: Callable = func():
 	pass
+
+func set_highlight(enabled: bool) -> void:
+	if sprite and sprite.material:
+		sprite.material.set_shader_parameter("outline_enabled", enabled)
